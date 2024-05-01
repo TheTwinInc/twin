bf9db4f0eb10c4ffd801993a2d01eed94761f700
Sergio Uribe <suribes@users.noreply.github.com>
2024-05-01 19:35:44 +0200

    Add logout, callback routes

--- src/app/twin/src/app/routes/app.routes.ts
+++ src/app/twin/src/app/routes/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from "@angular/router";
 
 // import { AppHomeComponent } from './home';
-import { AppHomeComponent, LoginComponent, RegisterComponent } from '../components';
+// import { AppHomeComponent, LoginComponent, LogoutComponents, RegisterComponent, CallbackComponent } from '../components';
+import { AppHomeComponent, LoginComponent, LogoutComponent, RegisterComponent, CallbackComponent } from '@app/components';
 import { authGuard } from '../helpers';
+import { authGuardService } from '@app/services';
 
 const usersRoutes = () => import('./users.routes').then(x => x.USERS_ROUTES);
 
 export const APP_ROUTES: Routes = [
-    { path: '', component: AppHomeComponent, canActivate: [authGuard] },
-    { path: 'users', loadChildren: usersRoutes, canActivate: [authGuard] },
+    { path: '', component: AppHomeComponent, canActivate: [authGuardService] },
+    { path: 'users', loadChildren: usersRoutes, canActivate: [authGuardService] },
+    // { path: '', component: AppHomeComponent, canActivate: [authGuard] },
+    // { path: 'users', loadChildren: usersRoutes, canActivate: [authGuard] },
     { path: 'account/login', component: LoginComponent },
+    { path: 'account/logout', component: LogoutComponent },
     { path: 'account/register', component: RegisterComponent },
+    { path: 'account/callback', component: CallbackComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
