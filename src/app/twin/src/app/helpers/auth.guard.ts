b8dbd15fba8aeb8d0feb285b4b6595f7430c7b82
Sergio Uribe <suribes@users.noreply.github.com>
2024-05-01 19:26:44 +0200

    Cleanup authorization guard

--- src/app/twin/src/app/helpers/auth.guard.ts
+++ src/app/twin/src/app/helpers/auth.guard.ts
@@ -1,5 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { inject } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
@@ -18,23 +16,3 @@ export function authGuard(route: ActivatedRouteSnapshot, state: RouterStateSnaps
     router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
     return false;
 }
-
-// @Injectable({ providedIn: 'root' })
-// export class AuthGuard implements CanActivate {
-//     constructor(
-//         private router: Router,
-//         private accountService: AccountService
-//     ) { }
-
-//     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-//         const user = this.accountService.userValue;
-//         if (user) {
-//             // authorised so return true
-//             return true;
-//         }
-
-//         // not logged in so redirect to login page with the return url
-//         this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
-//         return false;
-//     }
-// }
