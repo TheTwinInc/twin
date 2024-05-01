b6f7e02af77f1ed9f921fd83b837df81e76253a7
Sergio Uribe <suribes@users.noreply.github.com>
2024-05-01 19:30:03 +0200

    Update app config tests

--- src/app/twin/src/app/services/app-config.service.spec.ts
+++ src/app/twin/src/app/services/app-config.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AppConfigService } from './app-config.service';
 
 describe('AppConfigService', () => {
-  let service: AppConfigService;
+    let service: AppConfigService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AppConfigService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        });
+        service = TestBed.inject(AppConfigService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
