7073bf1a8b019060ee06de0081d0bd9d9e86cdb4
Sergio Uribe <suribes@users.noreply.github.com>
2024-05-01 19:30:24 +0200

    Add solid, auth-guard services

--- src/app/twin/src/app/services/index.ts
+++ src/app/twin/src/app/services/index.ts
@@ -1,3 +1,5 @@
 export * from './account.service';
 export * from './app-config.service';
 export * from './alarm.service';
+export * from './solid-account.service';
+export * from './auth-guard.service';
