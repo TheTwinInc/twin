c6f864deb86d1e0358ee7ff25fd6fcee21aef008
Sergio Uribe <suribes@users.noreply.github.com>
2024-05-01 19:36:48 +0200

    Update with solid account services

--- src/app/twin/src/app/components/header/header.component.ts
+++ src/app/twin/src/app/components/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
-import { Router, ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { filter } from 'rxjs';
+// import { Router, ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 
-import { AccountService } from '../../services';
-import { User } from '../../models';
+import { AccountService, SolidAccountService } from '@app/services';
+import { User } from '@app/models';
+import { openLoginDialog } from '@app/components';
 
 @Component({
     selector: 'app-header',
@@ -24,11 +28,37 @@ import { User } from '../../models';
 export class HeaderComponent {
     user?: User | null;
 
-    constructor(private accountService: AccountService) {
+    constructor(
+        private accountService: AccountService,
+        private solidAccountService: SolidAccountService,
+        private dialog: MatDialog,
+    ) {
         this.accountService.user.subscribe(x => this.user = x);
     }
 
     logout() {
         this.accountService.logout();
+    }
+
+    // login() {
+    //     openLoginDialog(this.dialog)
+    //     .pipe(
+    //         filter((val: any) => !!val)
+    //     )
+    //     .subscribe(
+    //         (val: any) => {
+    //             console.log(`LD: Login please... ${JSON.stringify(val)}`);
+    //             // this.updateFlightDetails(val);
+    //         }
+    //     );
+    // }
+    login() {
+        this.solidAccountService.solidLogin();
+    }
+    logoutApp() {
+        this.solidAccountService.solidLogoutApp();
+    }
+    logoutIdp() {
+        this.solidAccountService.solidLogoutIdp();
     }
 }
