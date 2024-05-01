47585114ca4503638a67fb32b768df166da433b2
Sergio Uribe <suribes@users.noreply.github.com>
2024-05-01 19:38:27 +0200

    Cleanup app components

--- src/app/twin/src/app/app.component.ts
+++ src/app/twin/src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-// import { AlarmComponent, HeaderComponent, FooterComponent, AppSidebarComponent } from './components';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent, FooterComponent, AppSidebarComponent } from './components';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
-        NgIf,
         RouterOutlet,
-        RouterLink,
-        RouterLinkActive,
-        // AlarmComponent,
         HeaderComponent,
         FooterComponent,
         AppSidebarComponent
