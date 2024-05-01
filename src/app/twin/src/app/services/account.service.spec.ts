bb170927c69afdb7b7105077e839afe29a8ef2d6
Sergio Uribe <suribes@users.noreply.github.com>
2024-05-01 19:29:47 +0200

    Update account service tests

--- src/app/twin/src/app/services/account.service.spec.ts
+++ src/app/twin/src/app/services/account.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController  } from '@angular/common/http/testing';
 
 import { AccountService } from './account.service';
+import { User } from '@app/models';
 
 describe('AccountService', () => {
-  let service: AccountService;
+    let service: AccountService;
+    let httpTestingController: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AccountService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        });
+        service = TestBed.inject(AccountService);
+        httpTestingController = TestBed.inject(HttpTestingController);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    afterEach(() => {
+        // After every test, assert that there are no more pending requests.
+        httpTestingController.verify();
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    // it('can test HttpClient.get', () => {
+    //     const user: User = {username: 'test', password: 'test'};
+    //     const id = "1";
+    //     let loggedIn = false;
+      
+    //     service.login('test', 'test')
+    //         .subscribe({
+    //             next: () => {
+    //                 loggedIn = true;
+    //             }
+    //         });
+      
+    //     // Assert that the request is a GET.
+    //     expect(loggedIn).toEqual(true);
+    // });
 });
