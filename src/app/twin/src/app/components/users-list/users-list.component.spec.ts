b4b50a3630aeb94534e8b93a6e9f4d08b03c4cad
Sergio Uribe <suribes@users.noreply.github.com>
2024-05-01 19:23:26 +0200

    Update users list test

--- src/app/twin/src/app/components/users-list/users-list.component.spec.ts
+++ src/app/twin/src/app/components/users-list/users-list.component.spec.ts
@@ -1,23 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { UsersListComponent } from './users-list.component';
 
 describe('UsersListComponent', () => {
-  let component: UsersListComponent;
-  let fixture: ComponentFixture<UsersListComponent>;
+    let component: UsersListComponent;
+    let fixture: ComponentFixture<UsersListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [UsersListComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(UsersListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [UsersListComponent, HttpClientTestingModule],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {
+                            params: {
+                                id: 1,
+                            },
+                        },
+                    },
+                },
+            ]
+        })
+        .compileComponents();
+        
+        fixture = TestBed.createComponent(UsersListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
