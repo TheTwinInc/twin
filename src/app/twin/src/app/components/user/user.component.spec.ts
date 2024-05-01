2de17c7336c7685f5297300ef11757d331a19747
Sergio Uribe <suribes@users.noreply.github.com>
2024-05-01 19:23:13 +0200

    Update user test

--- src/app/twin/src/app/components/user/user.component.spec.ts
+++ src/app/twin/src/app/components/user/user.component.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+// import { MockBuilder, MockInstance, MockRender } from 'ng-mocks';
 
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
-  let component: UserComponent;
-  let fixture: ComponentFixture<UserComponent>;
+    let component: UserComponent;
+    let fixture: ComponentFixture<UserComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [UserComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(UserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                UserComponent,
+                HttpClientTestingModule
+            ],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {
+                            params: {
+                                id: 1,
+                            },
+                        },
+                    },
+                },
+            ]
+        })
+        .compileComponents();
+        
+        fixture = TestBed.createComponent(UserComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
