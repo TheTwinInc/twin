8c7f884a7d7aa5a86fcfba06e21deda890057adf
Sergio Uribe <suribes@users.noreply.github.com>
2025-05-27 18:55:40 +0200

    Cleanup

--- src/app/twin/src/app/services/auth-guard.service.ts
+++ src/app/twin/src/app/services/auth-guard.service.ts
@@ -1,17 +1,16 @@
 import { inject } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-import { SolidService, LoggerService } from '@app/services';
+import { SolidAuthService, LoggerService } from '@app/services';
 
 export function authGuardService(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const router = inject(Router);
-    const accountService = inject(SolidService);
+    const accountService = inject(SolidAuthService);
     const logger = inject(LoggerService);
     const sessionInfo = accountService.sessionInfoValue;
     let userAuthorised = false;
     
     if (sessionInfo?.isLoggedIn) {
-        logger.debug(`Session login: ${sessionInfo?.isLoggedIn}`)
         // authorised so return true
         userAuthorised =  true;
     } else {
