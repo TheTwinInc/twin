709b3bd29239ec967d24bb03bb3e4ee044422801
Sergio Uribe <suribes@users.noreply.github.com>
2025-06-01 12:42:24 +0200

    Update footer selector

--- src/app/twin/src/app/components/layout/footer/footer.component.ts
+++ src/app/twin/src/app/components/layout/footer/footer.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { AlarmComponent } from '@app/components';
 
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [AlarmComponent],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+    selector: 'footer',
+    standalone: true,
+    imports: [AlarmComponent],
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
