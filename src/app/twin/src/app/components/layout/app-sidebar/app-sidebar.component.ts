9a27c6ac5bf8db08b8201e61b59c83ded68e3afb
Sergio Uribe <suribes@users.noreply.github.com>
2025-06-01 12:43:25 +0200

    Cleanup sidebar component

--- src/app/twin/src/app/components/layout/app-sidebar/app-sidebar.component.ts
+++ src/app/twin/src/app/components/layout/app-sidebar/app-sidebar.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { NavTreeComponent } from "../nav-tree/nav-tree.component";
-// import { AlarmComponent } from '../../components/alarm/alarm.component';
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [NavTreeComponent],
-  // imports: [AlarmComponent],
-  templateUrl: './app-sidebar.component.html',
-  styleUrl: './app-sidebar.component.css'
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [],
+    templateUrl: './app-sidebar.component.html',
+    styleUrl: './app-sidebar.component.css'
 })
 export class AppSidebarComponent {
 
