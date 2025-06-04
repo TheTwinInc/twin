8a5293f2a7fb5dc1cc12527dc67f5e9b5fa5ff75
Sergio Uribe <suribes@users.noreply.github.com>
2025-06-04 10:27:59 +0200

    Update home location

--- src/app/twin/src/app/app.component.ts
+++ src/app/twin/src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent, FooterComponent, AppSidebarComponent, NavComponent, NavTreeComponent } from '@app/components';
+import { AppHomeComponent } from "@app/components";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [
-        RouterOutlet,
-        HeaderComponent,
-        FooterComponent,
-        AppSidebarComponent,
-        NavComponent,
-        NavTreeComponent
-    ],
+    imports: [AppHomeComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
--- src/app/twin/src/app/components/layout/app-home/app-home.component.ts
+++ src/app/twin/src/app/components/layout/app-home/app-home.component.ts
@@ -8,11 +8,19 @@ import { MatIconModule } from '@angular/material/icon';
 
 import { AlarmService, SolidAuthService, LoggerService } from '@app/services';
 import { SolidDataset } from '@inrupt/solid-client';
+import { RouterOutlet } from '@angular/router';
+import { AppSidebarComponent, FooterComponent, HeaderComponent, NavTreeComponent } from '@app/components';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [NgIf, NgFor, MatCardModule, MatIconModule],
+    imports: [
+        RouterOutlet,
+        HeaderComponent,
+        FooterComponent,
+        AppSidebarComponent,
+        NavTreeComponent,
+    ],
     templateUrl: './app-home.component.html',
     styleUrl: './app-home.component.css'
 })
@@ -23,8 +31,6 @@ export class AppHomeComponent implements OnInit, OnDestroy {
 
     constructor(
         private solidAuthService: SolidAuthService,
-        private alarmService: AlarmService,
-        private logger: LoggerService,
     ) { }
     
     ngOnInit() {
@@ -32,7 +38,6 @@ export class AppHomeComponent implements OnInit, OnDestroy {
             .pipe(takeUntil(this.ngUnsubscribe))
             .subscribe(x => {
                 this.sessionInfo = x;
-                // this.onSessionChange(this.sessionInfo);
             });
         this.solidAuthService.handleSessionRestore();
         this.dataItems = [];
@@ -42,58 +47,6 @@ export class AppHomeComponent implements OnInit, OnDestroy {
         this.ngUnsubscribe.next(null);
         this.ngUnsubscribe.unsubscribe();
     }
-
-    // async onSessionChange (sessionInfo: any) {
-    //     if (sessionInfo?.isLoggedIn) {
-    //         const pods = await this.solidService.getAllPods(sessionInfo.webId);
-    //         this.logger.info(`PODS: ${JSON.stringify(pods)}`);
-
-    //         // this.dataSetContent = JSON.stringify(pods);
-
-    //         // if (undefined != pods[0] ) {
-    //         let readingListUrl = '';
-    //         if (undefined != pods && pods.length >= 0 ) {
-    //             readingListUrl = `${pods[0]}getting-started/readingList/myList`;
-    //         }
-            
-    //         let myReadingList: SolidDataset;
-
-    //         try {
-    //             // Attempt to retrieve the reading list in case it already exists.
-    //             myReadingList = await this.solidService.getDataset(readingListUrl);
-    //             // Clear the list to override the whole list
-    //             let items = this.solidService.getThingAll(myReadingList);
-    //             // this.dataSetContent += JSON.stringify(items);
-    //             // items.forEach((item) => {
-    //             //     myReadingList = removeThing(myReadingList, item);
-    //             // });
-    //             let listcontent = "";
-    //             for (let i = 0; i < items.length; i++) {
-    //                 let item = this.solidService.getStringNoLocale(items[i]);
-    //                 if (item !== null && Object.keys(item).length !== 0) {
-    //                     // listcontent += JSON.stringify(item) + "\n";
-    //                     this.dataItems?.push(JSON.stringify(item));
-    //                 }
-    //             }
-    //             // this.dataItems += JSON.stringify(listcontent);
-    //         } catch (error) {
-    //             // if (typeof error?.statusCode === "number" && error.statusCode === 404) {
-    //             //     // if not found, create a new SolidDataset (i.e., the reading list)
-    //             //     myReadingList = createSolidDataset();
-    //             // } else {
-    //                 console.error(error);
-    //         }
-    //         // pods?.forEach(pod => {
-    //         //     let podOption = {};
-    //         //     podOption.textContent = pod.toString();
-    //         //     podOption.value = pod.toString();
-                
-    //         // });
-    //         // let response = await this.solidService.getDataSet("https://thetwin.stage.graphmetrix.net/");
-    //         // this.dataSetContent = JSON.stringify(response);
-    //         // this.logger.info(`DATASET: ${JSON.stringify(dataSet)}`);
-    //     }
-    // }
 }
     
 
