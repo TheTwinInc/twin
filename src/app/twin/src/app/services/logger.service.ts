2867b1a02c10dc224070bc3dc906aaa902197a45
Sergio Uribe <suribes@users.noreply.github.com>
2025-06-05 21:57:39 +0200

    Add send log to server

--- src/app/twin/src/app/services/logger.service.ts
+++ src/app/twin/src/app/services/logger.service.ts
@@ -1,13 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LogLevel } from '@app/models';
+import { AppConfigService } from '@app/services';
+import { catchError, Observable, of } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class LoggerService {
     logLevel: LogLevel = new LogLevel();
+
+    serverLogUri!: string;
+
+    httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    httpPayloadOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' })
+    };
     
-    constructor() {}
+    constructor(
+        private http: HttpClient,
+    ) {}
     
     info(msg: any): void {
         this.logWith(this.logLevel.Info, msg);
@@ -41,5 +56,27 @@ export class LoggerService {
                 
             }
         }
+    }
+
+    private sendToServer(level: string, message: string) {
+        const uri = `${AppConfigService.settings.apiUrl}/api/logs`;
+
+        let payload = new URLSearchParams();
+        payload.set('logEntry', message);
+
+        this.http.post<any>(uri, payload, this.httpPayloadOptions)
+        .pipe(
+            catchError(this.handleError<void>())
+        );
+        // .subscribe();
+    }
+    
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+            error(`${operation} failed: ${error.message}`);
+        
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
     }
 }
