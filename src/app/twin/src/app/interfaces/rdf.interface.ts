6bbd69d2e140956b30a71067545a734ada41f6c6
Sergio Uribe <suribes@users.noreply.github.com>
2025-06-20 15:57:03 +0200

    Add contact profile interface

--- src/app/twin/src/app/interfaces/rdf.interface.ts
+++ src/app/twin/src/app/interfaces/rdf.interface.ts
@@ -9,7 +9,34 @@ export interface RdfProfile {
     webId: string;
     name?: string;
     email?: string;
-    photo?: string;
-    // role?: string;
+    img?: string;
+    role?: string;
+    org?: string;
+    phone?: string;
     [key: string]: string | undefined;
+}
+
+export interface FriendProfile {
+    webId: string;
+    name: string;
+    email?: string;
+    img?: string;
+    role?: string;
+    org?: string;
+    // img: string | null;
+}
+
+export interface ProfileData {
+    name: string;
+    img: string | null;
+    org: string | null;
+    role: string | null;
+}
+
+export interface VCardData {
+    fullName: string;
+    org?: string;
+    title?: string;
+    email?: string;
+    phone?: string;
 }
