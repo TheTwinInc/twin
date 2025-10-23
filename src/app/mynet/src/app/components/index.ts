71c0bb197c5105f03a80cb817519d82a861139dc
Sergio Uribe <suribes@users.noreply.github.com>
2025-10-23 16:27:51 +0200

    Update components index

---
+++ src/app/mynet/src/app/components/index.ts
@@ -0,0 +1,17 @@
+export * from './utils/alarm/alarm.component';
+export * from './utils/qrcode/qrcode.component';
+export * from './utils/size-detector/size-detector.component';
+export * from './layout/header/header.component';
+export * from './layout/footer/footer.component';
+export * from './layout/app-home/app-home.component';
+export * from './account/callback/callback.component';
+export * from './account/onboarding/onboarding.component';
+export * from './profile/profile-knows-editor/profile-knows-editor.component';
+export * from './profile/profile-knows-list/profile-knows-list.component';
+export * from './profile/profile-thumbnail-picker/profile-thumbnail-picker.component';
+export * from './profile/profile-card/profile-card.component';
+export * from './profile/profile-home/profile-home.component';
+export * from './nodes/node/node.component';
+
+
+
