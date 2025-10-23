66a937b22eabfa5d363ae12782c4a09c513b2269
Sergio Uribe <suribes@users.noreply.github.com>
2025-10-23 16:25:36 +0200

    Add cordova build

---
+++ src/app/mynet/cordova/www/chunk-I7EIMKEY.js
@@ -0,0 +1,1 @@
+import{ma as o,na as t}from"./chunk-TWVN2LUL.js";import"./chunk-GUAWKLZI.js";var e=[{path:"",component:t},{path:"add",component:o},{path:"edit/:id",component:o}];export{e as USERS_ROUTES};
